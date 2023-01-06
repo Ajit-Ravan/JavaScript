04eb57a6b40556f049ac619c773068f840d8f5c7
Ajit-Ravan <84119408+Ajit-Ravan@users.noreply.github.com>
2023-01-06 13:05:48 +0530

    add save button functionalities

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -3,18 +3,31 @@
 let count = 0;
 
 let countEl = document.getElementById("count_el")  //pass in argument
+let saveEl = document.getElementById("save-el");
+
+
 function increment()
 {
     console.log("clicked");
-    count = count + 1;
-    countEl.innerText = count;
-    console.log(count);
-    
+    count += 1;
+    // count = count + 1;
+    // countEl.innerText = count; 
+    countEl.textContent = count; 
 }
 
+
+
+ 
 function save()
 {
-    console.log("save button is clicked");
+
+    let countStr = count + " - ";
+    // saveEl.innerText += countStr;
+    saveEl.textContent += countStr;
+    count = 0;                          //after clicked save the count will become 0
+    countEl.textContent = count;
+    
+    
 }
 
  
